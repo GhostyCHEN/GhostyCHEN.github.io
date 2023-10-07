9364909e3b3d71b632b73cb3dc7e5cfeffded7bc
yinjie.chen <1761038183@qq.com>
2023-10-07 09:42:12 +0800

    push

---
+++ chenyinjie_blog/.vuepress/config.ts
@@ -0,0 +1,91 @@
+import { defineUserConfig } from "vuepress";
+import type { DefaultThemeOptions } from "vuepress";
+import recoTheme from "vuepress-theme-reco";
+
+export default defineUserConfig({
+  title: "Yinjie_Chen's Blog",
+  description: "一个展示自我的静态平台",
+  head: [["link", { rel: "icon", href: "/logo.png" }]],
+  theme: recoTheme({
+    style: "@vuepress-reco/style-default",
+    logo: "/logo.png",
+    author: "yinjie_chen",
+    authorAvatar: "/head.png",
+    docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
+    docsBranch: "main",
+    docsDir: "example",
+    lastUpdatedText: "",
+    // series 为原 sidebar
+    series: {
+      "/docs/theme-reco/": [
+        {
+          text: "module one",
+          children: ["home", "theme"],
+        },
+        {
+          text: "module two",
+          children: ["api", "plugin"],
+        },
+      ],
+    },
+    navbar: [
+      { text: "Home", link: "/" },
+      { text: "Categories", link: "/categories/reco/1/" },
+      { text: "Tags", link: "/tags/tag1/1/" },
+    ],
+    // bulletin: {
+    //   body: [
+    //     {
+    //       type: "text",
+    //       content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
+    //       style: "font-size: 12px;",
+    //     },
+    //     {
+    //       type: "hr",
+    //     },
+    //     {
+    //       type: "title",
+    //       content: "QQ 群",
+    //     },
+    //     {
+    //       type: "text",
+    //       content: `
+    //       <ul>
+    //         <li>QQ群1：1037296104</li>
+    //         <li>QQ群2：1061561395</li>
+    //         <li>QQ群3：962687802</li>
+    //       </ul>`,
+    //       style: "font-size: 12px;",
+    //     },
+    //     {
+    //       type: "hr",
+    //     },
+    //     {
+    //       type: "title",
+    //       content: "GitHub",
+    //     },
+    //     {
+    //       type: "text",
+    //       content: `
+    //       <ul>
+    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
+    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
+    //       </ul>`,
+    //       style: "font-size: 12px;",
+    //     },
+    //     {
+    //       type: "hr",
+    //     },
+    //     {
+    //       type: "buttongroup",
+    //       children: [
+    //         {
+    //           text: "打赏",
+    //           link: "/docs/others/donate.html",
+    //         },
+    //       ],
+    //     },
+    //   ],
+    // },
+  }),
+});
---
+++ chenyinjie_blog/README.md
@@ -0,0 +1,31 @@
+---
+home: true
+modules:
+  - BannerBrand
+  - Blog
+  - MdContent
+  - Footer
+bannerBrand:
+  bgImage: "/bg.svg"
+  title: Yinjie_Chen's Blog
+  description: 思考🤔 记录📝 分享😘 尝试🥳
+  tagline: 我对自己的要求很低：我活在世上，无非想要明白些道理，遇见些有趣的事。倘能如我愿，我的一生就算成功。
+  buttons:
+    - { text: Guide, link: "/docs/guide/introduce" }
+    - {
+        text: Default Style,
+        link: "/docs/style-default-api/introduce",
+        type: "plain",
+      }
+  socialLinks:
+    - {
+        icon: "LogoGithub",
+        link: "https://github.com/vuepress-reco/vuepress-theme-reco",
+      }
+blog:
+  socialLinks:
+    - { icon: "LogoGithub", link: "https://github.com/recoluan" }
+isShowTitleInHome: true
+actionText: About
+actionLink: /views/other/about
+---
